7a2b6843d1eb997fed86dff2fad072501ed949a0
Nhứt Nguyên <kieuphuongcsgo@gmail.com>
2022-06-16 09:23:10 +0700

    master

--- events/ready.js
+++ events/ready.js
@@ -1,5 +1,5 @@
 module.exports = (client) => {
     client.logger.success(`${client.user.username} đã sẵn sàng!`);
     client.user.setPresence({ activities: [{ name: 'DiscordJS testing'}], status: 'online'} );
-    client.logger.info(`${client.user.username} is running on ${client.guilds.cache.size} server(s)`);
+    client.logger.info(`${client.user.username} đang hoạt động trong ${client.guilds.cache.size} Server`);
 }
